0ea940f76a8a3920d38f2e57ef0fe5dbfeecc2b7
Ligo <1034007552@qq.com>
2020-04-13 09:12:22 +0800

    第二次

---
+++ js/mock-2.js
@@ -0,0 +1,9 @@
+var data=Mock.mock('http:www.clg.com',{
+    "result|20":[{
+        "title":'@csentence',
+        "body":'@cparagraph',
+        "name":'@cname',
+        "city":"@city",
+        "ctime":'@datetime("y-MM-dd HH:mm:ss")'
+    }]
+})

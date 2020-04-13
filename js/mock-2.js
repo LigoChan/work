d67a5412c892658fbc0edbd2113d63149df5125d
Ligo <1034007552@qq.com>
2020-04-13 21:17:49 +0800

    第二次

--- js/mock-2.js
+++ js/mock-2.js
@@ -1,5 +1,6 @@
 var data=Mock.mock('http:www.clg.com',{
     "result|20":[{
+        "id|+1":1,
         "title":'@csentence',
         "body":'@cparagraph',
         "name":'@cname',
@@ -7,3 +8,15 @@ var data=Mock.mock('http:www.clg.com',{
         "ctime":'@datetime("y-MM-dd HH:mm:ss")'
     }]
 })
+//拆分&
+function getUrlName() { 
+    var obj={}
+    var url=location.search.length>0?location.search.substring(1):''
+    var items=url.split('&')
+    var item=null
+    for(var i=0;i<items.length;i++){
+       item=items[i].split('=')
+       obj[item[0]]=item[1]
+    }
+    return obj
+ }
